f8015265cf8b13055d7948ccc4add95523314e4e
khaledyousryhegazy <yousryk49@gmail.com>
2026-07-13 17:28:03 +0300

    finish multi env pipelines

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -6,6 +6,14 @@ terraform {
       version = ">= 6.0"
     }
   }
+
+  backend "s3" {
+    region         = var.region
+    bucket         = "terraform-state-604275788373-us-east-1-an"
+    dynamodb_table = "terraform-state-lock"
+    key            = "multi-env-cicd-platform/terraform.tfstate"
+    use_lockfile   = true
+  }
 }
 
 provider "aws" {
